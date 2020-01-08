d6aa066ab6ceb6011fa30835a389526f20d8d383
omar <omarzuluaga123@hotmail.com>
2020-01-08 09:47:05 -0500

    added estudiante service and addapter

---
+++ src/main/java/com/example/estudiante/dominio/service/EstudianteService.java
@@ -0,0 +1,15 @@
+package com.example.estudiante.dominio.service;
+
+import java.util.List;
+
+import com.example.estudiante.dominio.models.Estudiante;
+import com.example.estudiante.shared.dominio.Id;
+
+public interface EstudianteService {
+	public List<Estudiante> buscarTodo();
+	public Estudiante buscarPorId(Id codigo);
+	public List<Estudiante> buscarPorIds(List<Id> codigos);
+	public void guardar(Estudiante estudiante);
+	public void eliminar(Id codigo);
+	public void actualizar(Estudiante estudiante);
+}
---
+++ src/main/java/com/example/estudiante/infraestructura/repositroy/adapters/EstudianteAdappter.java
@@ -0,0 +1,56 @@
+package com.example.estudiante.infraestructura.repositroy.adapters;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.estudiante.dominio.models.Estudiante;
+import com.example.estudiante.dominio.service.EstudianteService;
+import com.example.estudiante.infraestructura.mapper.EstudianteMapper;
+import com.example.estudiante.infraestructura.repositroy.database.EstudianteRepository;
+import com.example.estudiante.shared.dominio.Id;
+
+public class EstudianteAdappter implements EstudianteService{
+	
+	@Autowired
+	public EstudianteRepository estudianteRepository;
+	
+	@Autowired
+	public EstudianteMapper estudianteMapper;
+
+	@Override
+	public List<Estudiante> buscarTodo() {
+		
+		return estudianteMapper.listDtoToDominio(estudianteRepository.findAll());
+	}
+
+	@Override
+    public Estudiante buscarPorId(Id codigo) {
+		return estudianteMapper.dtoToDominio(estudianteRepository.findById(codigo.getValue()).get());
+	}
+
+	@Override
+	public List<Estudiante> buscarPorIds(List<Id> codigos) {
+		return estudianteMapper.listDtoToDominio(estudianteRepository.findAllById(
+				codigos.stream().map(codigo -> codigo.getValue()).collect(Collectors.toList())));
+	}
+
+	@Override
+	public void guardar(Estudiante estudiante) {
+		estudianteRepository.save(estudianteMapper.dominioToDto(estudiante));
+	}
+
+	@Override
+	public void eliminar(Id codigo) {
+		estudianteRepository.deleteById(codigo.getValue());
+		
+	}
+
+	@Override
+	public void actualizar(Estudiante estudiante) {
+		estudianteRepository.save(estudianteMapper.dominioToDto(estudiante));
+		
+	}
+
+}
